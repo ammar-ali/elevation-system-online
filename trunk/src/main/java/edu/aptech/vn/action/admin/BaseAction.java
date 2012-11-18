ab483293881e999208a0651ab3aa00b5a6dbdf9a
longprao@gmail.com <longprao@gmail.com@757020cb-3e7f-fd36-b2ac-4bd386ecc4de>
2012-11-18 09:42:36 +0000

    * hibernate

---
+++ trunk/src/main/java/edu/aptech/vn/action/admin/BaseAction.java
@@ -0,0 +1,12 @@
+package edu.aptech.vn.action.admin;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * All actions must extend this!!!
+ * User: LongDH
+ * Date: 11/16/12
+ * Time: 7:13 PM
+ */
+public abstract class BaseAction extends ActionSupport {
+}
---
+++ trunk/src/main/java/edu/aptech/vn/utils/Const.java
@@ -0,0 +1,10 @@
+package edu.aptech.vn.utils;
+
+/**
+ * User: LongDH
+ * Date: 11/16/12
+ * Time: 5:47 PM
+ */
+public interface Const {
+    String DB = "elevation";
+}
