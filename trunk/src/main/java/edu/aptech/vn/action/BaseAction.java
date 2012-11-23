48ea3fc2db62e60c0d2abd93f01e6cf2dede0d43
longprao@gmail.com <longprao@gmail.com@757020cb-3e7f-fd36-b2ac-4bd386ecc4de>
2012-11-23 12:19:05 +0000

    * login + new sql + order list

--- trunk/src/main/java/edu/aptech/vn/action/BaseAction.java
+++ trunk/src/main/java/edu/aptech/vn/action/BaseAction.java
@@ -4,9 +4,13 @@ import com.opensymphony.xwork2.ActionSupport;
 import edu.aptech.vn.utils.HibernateUtil;
 import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.convention.annotation.Results;
+import org.apache.struts2.interceptor.ParameterAware;
 import org.apache.struts2.interceptor.SessionAware;
 import org.hibernate.Session;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,9 +23,10 @@ import java.util.Map;
 @Results({
     @Result(name="error", location="/error.jsp")
 })
-public abstract class BaseAction extends ActionSupport implements SessionAware {
+public abstract class BaseAction extends ActionSupport implements SessionAware, ParameterAware {
     private int id;
     private Map<String, Object> session = new HashMap<String, Object>();
+    private Map<String, String[]> params = new HashMap<String, String[]>();
     protected final Session db = HibernateUtil.getSession().openSession();
 
     protected String title = getClass().getName().substring(getClass().getName().lastIndexOf(".") + 1, getClass().getName().indexOf("Action"));
@@ -49,5 +54,34 @@ public abstract class BaseAction extends ActionSupport implements SessionAware {
 
     public Map<String, Object> getSession() {
         return session;
+    }
+
+    @Override
+    public void setParameters(Map<String, String[]> params) {
+        this.params = params;
+    }
+
+    public String getParam(String param) {
+        return ((String[])params.get(param))[0];
+    }
+
+    public String md5(String s) {
+        MessageDigest m = null;
+        try {
+            m = MessageDigest.getInstance("MD5");
+            m.reset();
+            m.update(s.getBytes());
+            byte[] digest = m.digest();
+            BigInteger bigInt = new BigInteger(1,digest);
+            String hashtext = bigInt.toString(16);
+// Now we need to zero pad it if you actually want the full 32 chars.
+            while(hashtext.length() < 32 ){
+                hashtext = "0"+hashtext;
+            }
+            return hashtext;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
